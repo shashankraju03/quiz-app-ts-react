46e71e45b77e27062a257b8d42f36a07907181ed
shashankraju03 <shashankrajuprathikantam@gmail.com>
2022-10-12 04:37:58 +0530

    Updates

---
+++ precache-manifest.037bbe476cd12d4183540b9153cab64c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e76c54e68a7c2a2931e4c0b27ab2660",
+    "url": "/quiz-app-ts-react/index.html"
+  },
+  {
+    "revision": "a8ea30f0a3041f282e5c",
+    "url": "/quiz-app-ts-react/static/js/2.606b0d1e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quiz-app-ts-react/static/js/2.606b0d1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d8aba86fb910fc5a75f",
+    "url": "/quiz-app-ts-react/static/js/main.cb066996.chunk.js"
+  },
+  {
+    "revision": "466fdbd7f7ad3ca11618",
+    "url": "/quiz-app-ts-react/static/js/runtime-main.2b274c3f.js"
+  },
+  {
+    "revision": "d6988bbc282be81c24e67d36ecc1cb6a",
+    "url": "/quiz-app-ts-react/static/media/bg.d6988bbc.png"
+  }
+]);
